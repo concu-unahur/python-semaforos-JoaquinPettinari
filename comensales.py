ca7e93055ad3eb0a06e2751f441838212d3bea71
JoaquinPettinari <joaquinpettinari@hotmail.com>
2020-02-13 21:26:25 -0300

    Para mejorar el ultimo

--- comensales.py
+++ comensales.py
@@ -5,26 +5,29 @@ import logging
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 semaforoCocinero = threading.Semaphore(0)
-semaforoComensal = threading.Semaphore(3)
+semaforoComensal = threading.Semaphore(1)
 platosDisponibles = 3
 
 class Cocinero(threading.Thread):
-  def __init__(self):
+  def __init__(self, numero):
     super().__init__()
-    self.name = 'Cocinero'
+    self.name = f'Cocinero {numero}'
   
   def run(self):
     global platosDisponibles
     global semaforoCocinero
     global semaforoComensal
 
-    semaforoCocinero.acquire()
 
-    platosDisponibles += 3
-    logging.info(f'Reponiendo los platos')  
-    semaforoComensal.release()
-    semaforoComensal.release()
-    semaforoComensal.release()
+    while(True):
+      semaforoCocinero.acquire()
+      try:
+        logging.info(f'Reponiendo los platos')  
+        platosDisponibles = 3
+      finally:
+        semaforoComensal.release()
+
+
 
 
 
@@ -35,19 +38,29 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
+    global semaforoCocinero
+    global semaforoComensal
 
+    semaforoComensal.acquire()
 
+    try:
+      if(platosDisponibles <= 0):
+        semaforoCocinero.release()
+        semaforoComensal.acquire()
+        platosDisponibles -= 1
+        logging.info(f' ¡Que rico! Quedan {platosDisponibles} platos')
 
-    if(platosDisponibles <= 0):
-      semaforoCocinero.release()
-      semaforoComensal.acquire() 
-    else:
-      platosDisponibles -= 1
-      logging.info(f' ¡Que rico! Quedan {platosDisponibles} platos')
-      semaforoComensal.acquire()
+      else:
+        platosDisponibles -= 1
+        logging.info(f' ¡Que rico! Quedan {platosDisponibles} platos')
+    
+    finally:
+      semaforoComensal.release()
   
 
-Cocinero().start()
+ 
+for i in range(2):
+  Cocinero(i).start()
 
 
 for i in range(10):
